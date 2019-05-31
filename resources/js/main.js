dfb504d8ab0da0f828cb7bd4ba0098d2d82a4e68
Michael Humphries <michaelhumphries1997@gmail.com>
2019-05-31 20:18:30 +0100

    adding map

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,15 +1,13 @@
-const axios = require("axios")
-
 let API_URL = "https://api.wheretheiss.at/v1/satellites/25544"
 
 async function get_data() {
-  let response = await axios.get(API_URL)
-  let data = await response.data
+  let response = await fetch(API_URL)
+  let data = await response.json()
   const { latitude, longitude } = data
   console.log(latitude)
   console.log(longitude)
-  document.getElementById("lat").value = latitude
-  document.getElementById("long").value = longitude
+  document.getElementById("lat").textContent = latitude
+  document.getElementById("long").textContent = longitude
 }
 
-get_data()
+//setInterval(get_data, 2000)
