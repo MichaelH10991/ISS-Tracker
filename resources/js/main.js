1fb938789ca765e45433fd0620790aa68b5eddaf
Michael Humphries <michaelhumphries1997@gmail.com>
2019-05-31 19:04:51 +0100

    adding js that retreives data from iss api

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,2 +1,15 @@
-let text = "Hello World"
-document.getElementById("demo").innerHTML = text
+const axios = require("axios")
+
+let API_URL = "https://api.wheretheiss.at/v1/satellites/25544"
+
+async function get_data() {
+  let response = await axios.get(API_URL)
+  let data = await response.data
+  const { latitude, longitude } = data
+  console.log(latitude)
+  console.log(longitude)
+  document.getElementById("lat").value = latitude
+  document.getElementById("long").value = longitude
+}
+
+get_data()
